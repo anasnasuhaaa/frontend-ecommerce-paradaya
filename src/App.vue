98f645e765f27a13c4f4ab6ad9e253e88f2c1a95
siti-nur-a <snuraisyah643@gmail.com>
2025-01-22 13:16:47 +0700

    feat: add Homepage & Product page

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,10 @@
 <script setup>
 import { RouterView } from "vue-router";
 import AdminaLyout from "./layouts/Admin.vue";
+import Header from "./components/Header.vue";
 </script>
 
 <template>
+  <Header />
   <RouterView></RouterView>
 </template>
---
+++ src/pages/user/Products.vue
@@ -0,0 +1,7 @@
+<template>
+  <ProductCard />
+</template>
+
+<script setup>
+import ProductCard from "../../components/ProductCard.vue";
+</script>
