0ebe4007d9cfdf70b0e2b4a6f44e2719c800298a
Anas Nasuha <infoanaspedia@gmail.com>
2025-01-23 14:30:52 +0700

    feat: add Admin panel, crud category & product

--- src/routes/route.js
+++ src/routes/route.js
@@ -2,19 +2,58 @@ import { createWebHistory, createRouter } from "vue-router";
 
 import HomeView from "../pages/user/Home.vue";
 // import AboutView from "./AboutView.vue";
-import AdminaLyout from "../layouts/Admin.vue";
 import ProductCard from "../components/ProductCard.vue";
 import DetailProduct from "../components/DetailProduct.vue";
 import Cart from "../pages/user/Cart.vue";
 import LoginForm from "../components/LoginForm.vue";
 
+import AdminLayout from "../layouts/Admin.vue";
+import Settings from "../pages/admin/Settings.vue";
+import Dashboard from "../pages/admin/Dashboard.vue";
+import Category from "../pages/admin/Category.vue";
+import Product from "../pages/admin/Product.vue";
+
 const routes = [
   { path: "/", component: HomeView },
-  { path: "/dashboard", component: AdminaLyout },
   { path: "/products", component: ProductCard },
   { path: "/detail-produk", component: DetailProduct },
   { path: "/cart", component: Cart },
   { path: "/login", component: LoginForm },
+  {
+    path: "/admin",
+    component: AdminLayout,
+    children: [
+      {
+        path: "dashboard",
+        component: Dashboard,
+      },
+      {
+        path: "category",
+        component: Category,
+      },
+      {
+        path: "product",
+        component: Product,
+      },
+      // {
+      //   path: "settings",
+      //   component: Settings,
+      // },
+      // {
+      //   path: "profile",
+      //   component: Profile,
+      // },
+      // {
+      //   path: "",
+      //   redirect: "/admin/dashboard", // Redirect default ke dashboard
+      // },
+    ],
+  },
+  {
+    path: "/settings",
+    name: "Settings",
+    component: Settings,
+  },
 ];
 
 const router = createRouter({
